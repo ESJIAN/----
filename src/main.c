354d02275af8cddc19c9e50886887752095299db
ESJIAN <3340580252@qq.com>
2025-10-22 13:28:55 +0800

    ``` feat(oled): 显示超声波测距结果并处理超出范围情况
    当距离大于等于400cm时，OLED显示"Out of range"；
    否则正常显示距离数值和单位"cm"。
    同时注释掉串口发送距离值的功能代码。
    ```

--- src/main.c
+++ src/main.c
@@ -19,15 +19,31 @@ void main()
     
     while (1) 
     {
+
         distance = Ultrasonic_GetDistance();
-        Int_OLED_ShowStr(0, 0, "Distance");
-        Int_OLED_ShowNum(10, 0, distance);
-        Int_OLED_ShowStr(13, 0, "cm");
+        if (distance >= 400) 
+        {
+            // 局部清屏
+            Int_OLED_ShowStr(0, 0, "                  ");
+            Int_OLED_ShowStr(13, 0, " ");
+            
+            Int_OLED_ShowStr(0, 1, "Out of range");
+        }
+        else
+        {
+            // 局部清屏
+            Int_OLED_ShowStr(0, 1, "                  ");
+
+            Int_OLED_ShowStr(0, 0, "Distance");
+            Int_OLED_ShowNum(9, 0, distance);
+            Int_OLED_ShowStr(13, 0, "cm");
+        }
+        
         
         // 通过串口发送距离值
 
-        Uart_SendHex((distance >> 8) & 0xFF);
-        Uart_SendHex(distance & 0xFF);
+        //Uart_SendHex((distance >> 8) & 0xFF);
+        //Uart_SendHex(distance & 0xFF);
         
     }
 }
